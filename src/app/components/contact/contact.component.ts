381a906bce868c1e408a3a5457bf218c9ae7a744
M-Ashraf-Elsayed <pop616631@gmail.com>
2025-01-26 22:47:56 +0200

    Make the label hide when the input be empty

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,31 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-  title1: boolean = false;
-  showInputTitle1(): void {
-    this.title1 = true;
-  }
-  title2: boolean = false;
-  showInputTitle2(): void {
-    this.title2 = true;
-  }
-  title3: boolean = false;
-  showInputTitle3(): void {
-    this.title3 = true;
-  }
-  title4: boolean = false;
-  showInputTitle4(): void {
-    this.title4 = true;
-  }
-  // submit: boolean = false;
-  // clickSubmit(e: MouseEvent): void {
-  //   if()
-  //   this.submit = true;
-  // }
+  userName: string = '';
+  userAge: string = '';
+  userEmail: string = '';
+  password: string = '';
 }
